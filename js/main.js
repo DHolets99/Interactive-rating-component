5f7f0fc2d1cf6299fbe640405d1beca7a8387f1b
DHolets99 <dholets99@gmail.com>
2022-06-08 22:44:47 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,14 +2,38 @@
 
 const ratingVariants = document.querySelector('.rating_variants');
 const ratingElements = document.querySelectorAll('.rating_el');
+const mark = document.querySelector('.mark');
+const form = document.querySelector('.rating');
+const mainScreen = document.querySelector('.main');
+const cardScreen = document.querySelector('.card');
+
 
 let addActiveClass = (e) =>{
     let target = e.target;
 
     ratingElements.forEach( (item) => item.classList.remove('rating_el-active'));
+
+    if (target != ratingVariants) {
     target.classList.toggle('rating_el-active');
+    }
+
+    mark.textContent = target.placeholder;
 }
 
 ratingVariants.addEventListener('click', addActiveClass);
 
+
 /* toggle raiting variants end */
+
+
+/* get mark from rating start*/
+
+let showMark = (e) => {
+    mainScreen.style = 'display: none';
+    cardScreen.style = 'display: block';
+    e.preventDefault();
+}
+
+form.addEventListener('submit', showMark)
+
+/* get mark from rating end*/
